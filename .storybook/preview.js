2a1fcbb4262ba93de292e9a2387f94825547abfa
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-01-19 16:08:46 +0900

    custom toolbar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -25,14 +25,28 @@ export const parameters = {
 
 export const decorators = [
   (story, context) => ({
+    props: {
+      padding: {
+        type: String,
+        default: context.globals.padding
+      }
+    },
     template: `
-      <div>
-        <h1>${context.kind}</h1>
-        <h2>args</h2>
-        <p>${JSON.stringify(context.args)}</p>
-        <h2>Reuslt</h2>
+      <div :style="{padding: padding}">
         <story />
       </div>
     `
   })
 ]
+
+export const globalTypes = {
+  padding: {
+    name: 'Padding',
+    description: 'ストーリー描画時のpaddingを設定します',
+    defaultValue: '0px',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['0px', '30px', '100px']
+    }
+  }
+}
